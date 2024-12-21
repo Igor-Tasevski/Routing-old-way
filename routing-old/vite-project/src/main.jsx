8d0a2aa7091796afbcbf8b10a9fcabbc0c62c950
Igor-Tasevski <igortasevski22@gmail.com>
2024-12-21 16:16:25 +0100

    Adding another project

---
+++ routing-old/vite-project/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+//import App from './App.jsx'
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Staticni stranici */}
+        <Route path="/" element={<div>Hello world</div>} />
+        <Route path="/about" element={<div>Hello about</div>} />
+        <Route path="/contact" element={<div>Hello contact</div>} />
+        {/* Dinamicna stranica */}
+        <Route path="/product/:id" element={<div>You did it</div>} />
+
+        <Route path="*" element={<div>ERROR</div>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
